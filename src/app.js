f02b9f815a63e18fa420c8a7ed3d575dabcc3c09
Natalia Manzano Carnerero <mcn.sdevelop@gmail.com>
2024-11-18 17:21:32 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,39 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronombre = ["la", "nuestra", "tu"];
+  let adjetivo = ["mejor", "nueva", "profesional"];
+  let nombre = ["ropa", "cafe", "musica"];
+  let dominio = [".com", ".net", ".es"];
+  let hackDomain = ["restaurant.es"];
+
+  let textForOf = "";
+  let textHackDomain = "";
+
+  for (const elementP of pronombre) {
+    for (const elementA of adjetivo) {
+      for (const elementN of nombre) {
+        for (const elementD of dominio) {
+          textForOf += `
+          <li class="list-group-item">
+            ${elementP}${elementA}${elementN}${elementD}
+          </li>`;
+        }
+      }
+    }
+  }
+  for (const elementP of pronombre) {
+    for (const elementA of adjetivo) {
+      for (const elementH of hackDomain) {
+        textHackDomain += `
+        <li class="list-group-item">
+          ${elementP}${elementA}${elementH} 
+        </li>`;
+      }
+    }
+  }
+  document.querySelector("#dominio").innerHTML = textForOf;
+
+  document.querySelector("#h-dominio").innerHTML = textHackDomain;
 };
